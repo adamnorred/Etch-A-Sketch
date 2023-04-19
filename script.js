0bd348dfff53d5f95ba5f641a6d0c2855f6423b9
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-19 22:27:52 +0200

    make grid function

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const gridContainerDom = document.querySelector(".container-main-grid");
+let userPrompt = 0;
 
-for (let element = 0; element < 19; element++) {
-  const gridElementDom = document.createElement("div");
-  gridElementDom.classList.add("grid-element");
-  gridContainerDom.appendChild(gridElementDom);
+function getUserPrompt() {
+  while (true) {
+    userPrompt = Number(prompt("grid size x*x?"));
+    if (userPrompt > 0 && userPrompt < 101) {
+      break;
+    } else {
+      alert("wrong input, has to be greater than 0, less than 101");
+    }
+  }
 }
+
+function makeGrid(number) {
+  for (let element = 0; element < number * number; element++) {
+    const gridElementDom = document.createElement("div");
+    gridElementDom.classList.add("grid-element");
+    gridElementDom.style.height = `calc(${100 / number}% - 2px)`;
+    gridElementDom.style.width = `calc(${100 / number}% - 2px)`;
+    gridContainerDom.appendChild(gridElementDom);
+  }
+}
+
+getUserPrompt();
+makeGrid(userPrompt);
+
+// check if there is proper grid element count
+const check = gridContainerDom.childElementCount;
