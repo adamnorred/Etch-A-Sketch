967c3c4e206b649f78315dc644931e3ab6ec6401
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-24 12:30:36 +0200

    remove grid function

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 const gridContainerSelector = document.querySelector(".container-main-grid");
-let userPrompt = 0;
-
-function getUserPrompt() {
-  while (true) {
-    userPrompt = Number(prompt("type x to make grid x*x"));
-    if (userPrompt > 0 && userPrompt < 101) {
-      break;
-    } else {
-      alert("wrong input, has to be greater than 0, less than 101");
-    }
-  }
-}
 
 function makeGrid(number) {
   for (let element = 0; element < number * number; element++) {
@@ -22,16 +10,33 @@ function makeGrid(number) {
   }
 }
 
-getUserPrompt();
-makeGrid(userPrompt);
+function removeGridChildNodes() {
+  while (gridContainerSelector.hasChildNodes()) {
+    gridContainerSelector.removeChild(gridContainerSelector.firstChild);
+  }
+}
 
-// check if there is proper grid element count
+makeGrid(1);
+
+// check in console if there is proper grid element count
 const check = gridContainerSelector.childElementCount;
 
+// slider
 const slider = document.querySelector(".slider");
 const sliderValue = document.querySelector(".slider-value");
-sliderValue.value = slider.value;
 
 slider.addEventListener("input", function () {
   sliderValue.value = this.value;
+  removeGridChildNodes();
+  makeGrid(this.value);
+});
+
+sliderValue.addEventListener("input", function () {
+  if (sliderValue.value >= 1 && sliderValue.value < 101) {
+    slider.value = this.value;
+    removeGridChildNodes();
+    makeGrid(this.value);
+  } else {
+    removeGridChildNodes();
+  }
 });
